21116ab13e19530cd9c79f28c16378ac49ce5705
Maurício João Baccin <mauriciobaccin@gmail.com>
2024-03-21 23:55:16 -0300

    adds support for special chars at regex - parse file

--- src/parseFile.ts
+++ src/parseFile.ts
@@ -1,14 +1,15 @@
 import allBosses from './constants/bosses';
 import ILine from './types/ILine';
 
-const regex = new RegExp(`^(\\d{2}\\.\\d{2}\\.\\d{4} \\d{2}:\\d{2}:\\d{2})\\s+(${allBosses.join('|')})\\s+(\\d{2}:\\d{2})(?!.*kill)\\s+([a-zA-Z]+)$`);
+const regex = new RegExp(`^(\\d{2}\\.\\d{2}\\.\\d{4} \\d{2}:\\d{2}:\\d{2})\\s+(${allBosses.join('|')})\\s+(\\d{2}:\\d{2})(?!.*kill)\\s+([\\w*\\u00a8À-ÖØ-öØ-ÿ]+)$`);
 
 export default function parseFile(file: string): ILine[] {
   const allLines = file.split('\n');
-  
+
   return allLines.reduce((acc: ILine[], cur: string) => {
     const match = cur.match(regex);
     if (match && match[4].trim() !== '') {
+      console.log("match[4]: ", match[4])
       const newLine: ILine = {
         boss: match[2],
         date: match[1],
@@ -16,6 +17,8 @@ export default function parseFile(file: string): ILine[] {
         firstBlood: match[4],
       }
       acc.push(newLine);
+    } else {
+      console.log("match[4]: ", match ? match[4] : '')
     }
     return acc;
   }, []);
